4374d6c32ab7b347762554682698044b944bd56a
whellcolor <bahansatekambing@gmail.com>
2026-05-14 19:49:17 +0700

    Create ERC20.sol

---
+++ contracts/ERC20.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WCCToken is ERC20, Ownable {
+
+    uint256 public constant MAX_SUPPLY = 1_000_000_000 ether;
+
+    constructor() ERC20("Whellcolor Cube Crypto", "WCC") Ownable(msg.sender) {
+        _mint(msg.sender, 100_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "MAX SUPPLY");
+        _mint(to, amount);
+    }
+}
