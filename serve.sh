064e0c4f704fbec925f91c807842713c1eee92dd
whellcolor <bahansatekambing@gmail.com>
2026-05-19 01:11:25 +0700

    Create serve.sh

---
+++ serve.sh
@@ -0,0 +1,4 @@
+twak serve [--rest] [--port <port>] [--host <host>] \
+           [--auto-lock <minutes>] [--password <pw>] \
+           [--x402] [--payment-amount <amount>] [--payment-asset <asset>] \
+           [--payment-chain <chain>] [--payment-recipient <address>]
