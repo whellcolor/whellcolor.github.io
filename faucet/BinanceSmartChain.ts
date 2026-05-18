2fb35c07f76103304cc1babeb7f5df595305275c
whellcolor <bahansatekambing@gmail.com>
2026-05-18 11:29:33 +0700

    Create BinanceSmartChain.ts

---
+++ faucet/BinanceSmartChain.ts
@@ -0,0 +1,6 @@
+import { TatumSDK, Network, BinanceSmartChain } from '@tatumio/tatum'
+
+const tatum = await TatumSDK.init<BinanceSmartChain>({network: Network.BINANCE_SMART_CHAIN})
+
+const balance = await tatum.rpc.getBalance
+('0x5E8Bade6E0BCe65807dB6327cB1D9EEb7C6A6A5B')
