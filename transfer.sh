82f2c5aaf1edca49db8116cf64be59f9d4a70b4e
whellcolor <bahansatekambing@gmail.com>
2026-05-19 01:00:54 +0700

    Create transfer.sh

---
+++ transfer.sh
@@ -0,0 +1,3 @@
+twak transfer --to <address> --amount <amount> --token <token> \
+              [--confirm-to <address>] [--max-usd <n>] [--skip-safety-check] \
+              [--password <pw>] [--json]
