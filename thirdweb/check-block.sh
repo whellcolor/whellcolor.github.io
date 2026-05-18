72beab6a295728d18a19de145756d3f53b615850
whellcolor <bahansatekambing@gmail.com>
2026-05-18 22:56:55 +0700

    Create check-block.sh

---
+++ thirdweb/check-block.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl https://rpc.buildbear.io/global-wolverine-ff979075 \
+     -X POST \
+     -H "Content-Type: application/json" \
+     -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
