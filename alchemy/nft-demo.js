a247fb7a11c291895e009269182a33df6cde3a82
whellcolor <bahansatekambing@gmail.com>
2026-05-18 23:42:58 +0700

    Create nft-demo.js

---
+++ alchemy/nft-demo.js
@@ -0,0 +1,3 @@
+curl --request GET \
+     --url 'https://eth-mainnet.g.alchemy.com/nft/v3/5P3go2AF1svTq0QqCOZUQ/getNFTsForOwner?owner=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045&withMetadata=true&pageSize=100' \
+     --header 'accept: application/json'
