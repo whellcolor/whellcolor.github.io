d2ec5a18cf0852f8a359a5ef2ece04940ca8cd55
whellcolor <bahansatekambing@gmail.com>
2026-05-19 06:07:54 +0700

    Create MainActivity.kt

---
+++ app/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.whellcolor.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadsImagesAutomatically = true
+        settings.allowFileAccess = true
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        webView.loadUrl("https://whellcolor.github.io/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
